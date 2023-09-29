c60d811d8275a4f3f74e55a97d04852f5c99ff23
alkachhirang <alkachhirang3@gmail.com>
2023-09-29 14:56:57 +0530

    changes

---
+++ assets/js/app1.js
@@ -0,0 +1,6 @@
+// navbar
+function opennav() {
+    document.getElementById("navbar").classList.toggle("end-0");
+    document.body.classList.toggle("overflow_hiddden")
+    document.getElementById("menubtn-icon").classList.toggle("cross");
+}
