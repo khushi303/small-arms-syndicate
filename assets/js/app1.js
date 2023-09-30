b4245bce2c2165d11875c161e6b1d05ab88beb76
alkachhirang <alkachhirang3@gmail.com>
2023-09-30 15:35:00 +0530

    changes

--- assets/js/app1.js
+++ assets/js/app1.js
@@ -4,3 +4,50 @@ function opennav() {
     document.body.classList.toggle("overflow_hiddden")
     document.getElementById("menubtn-icon").classList.toggle("cross");
 }
+// team-slider
+$('.team_slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow:3,
+    slidesToScroll:3,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 1366,
+            settings: {
+                slidesToShow:2.4,
+                slidesToScroll:2,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow:3,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 850,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 380,
+            settings: {
+                slidesToShow: 1.10,
+                slidesToScroll: 1,
+            }
+        },
+    ]
+});
